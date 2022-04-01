aec2e041a22042c728fcd83ed42eb38326d21aac
kenta <ktohara@gmail.com>
2022-04-01 02:09:58 -0400

    Add base functions

--- script.js
+++ script.js
@@ -1,79 +1,112 @@
-let firstNum = ''
-let secondNum = ''
-let currentOperator = ''
+// Variables
 
-const add = (a, b) => {
-  return a + b;
-};
+let firstNum = '';
+let secondNum = '';
+let currentOperator = null;
+let resetDisplay = false;
 
-const subtract = (a, b) => {
-  return a - b;
-};
-
-const multiply = (a, b) => {
-  return a * b;
-};
-
-const divide = (a, b) => {
-  return a / b;
-};
-
-const operate = (operator, a, b) => {
-  switch (operator) {
-    case '+': return add(a, b);
-    case '-': return subtract(a, b);
-    case 'x': return multiply(a, b);
-    case '÷': return divide(a, b);
-  };
-};
 const input = document.getElementById('input');
 const output = document.getElementById('output');
 const clearButton = document.getElementById('allclear');
+const inverse = document.getElementById('inverse');
+const decimal = document.getElementById('decimal');
+const backspace = document.getElementById('backspace');
+const equals = document.getElementById('equals');
+
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = Array.from(document.querySelectorAll('.operator'));
 const operatorArray = operatorButtons.map(operator => operator.innerHTML);
 
-clearButton.addEventListener('click', clear)
+// Event Listeners
 
 numberButtons.forEach((button) =>
-  button.addEventListener('click', () => attachNumber(button.innerText))
-)
+  button.addEventListener('click', () => attachNumber(button.innerHTML))
+);
 
 operatorButtons.forEach((button) =>
-  button.addEventListener('click', () => attachOperator(button.innerText))
-)
+  button.addEventListener('click', () => attachOperator(button.innerHTML))
+);
+
+clearButton.addEventListener('click', clear);
+inverse.addEventListener('click', invertSign);
+decimal.addEventListener('click', attachDecimal);
+equals.addEventListener('click', evaluate);
+
+// Event Listener Functions
 
 function attachNumber(number) {
-  if (output.innerText === '0') {
+  if (output.innerHTML === '0' || resetDisplay === true) {
     resetOutput();
   }
-  output.innerText += number
+  output.innerHTML += number;
+  checkOutputLength()
+};
+
+function checkOutputLength() {
+  outputTotal = output.innerHTML;
+  length = outputTotal.length;
+  if (length > 9) {
+    output.innerHTML = outputTotal.substring(0, 9);
+  }
 }
 
 function attachOperator(operator) {
+  if (currentOperator !== null) evaluate();
+  firstNum = output.innerHTML;
   currentOperator = operator;
-  if (operatorArray.includes(currentOperator)) evaluate();
-  firstNum = output.innerText;
-  input.innerText = `${firstNum} ${currentOperator}`
-}
+  input.innerHTML = `${firstNum} ${currentOperator}`
+  resetDisplay = true;
+};
 
 function clear() {
   input.innerHTML = ''
   output.innerHTML = '0'
   firstNum = ''
   secondNum = ''
+  currentOperator = null
+};
+
+function invertSign() {
+  output.innerHTML *= -1;
 }
 
+function attachDecimal() {
+  if (!output.innerHTML.includes('.')) {
+    output.innerHTML += '.'
+  }
+}
+
+// Helper Functions
+
+// evaluates the current input and output with the current operator
 function evaluate() {
+  if (firstNum === '') return;
   secondNum = output.innerHTML;
-  output.innerHTML = operate(currentOperator, firstNum, secondNum);
-  resetInput()
+  const returnVal = operate(currentOperator, firstNum, secondNum);
+  output.innerHTML = roundNum(returnVal);
+};
+
+function roundNum(num) {
+  return Math.round((num + Number.EPSILON) * 100000) / 100000;
 }
 
 function resetInput() {
-  input.innerHTML = ''
-}
+  input.innerHTML = '';
+};
 
 function resetOutput() {
-  output.innerText = ''
-}
+  output.innerHTML = '';
+  resetDisplay = false;
+};
+
+const operate = (operator, a, b) => {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case 'MOD': return a % b;
+    case '+': return a + b;
+    case '-': return a - b;
+    case 'x': return a * b;
+    case '÷': return a / b;
+  };
+};
